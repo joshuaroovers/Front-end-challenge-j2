37564baf56dc18048ab4f28f8afa0af49f062454
Joshua <Doerak1aug@gmail.com>
2021-02-10 12:27:20 +0100

    start

---
+++ java.js
@@ -0,0 +1,40 @@
+partij = {"A" : 0, "B" : 0, "C" : 0, "D" : 0}
+
+voorbeeld = "A D";
+x = voorbeeld;
+
+function eens(x)
+{
+    y = x.split(" ");
+    z = 0;
+    foreach(y)
+    {
+        z++
+        partij[z]++;
+    }
+}
+eens();
+
+function eens(x)
+{
+    y = x.split(" ");
+    z = 0;
+    foreach(y)
+    {
+        z++
+        partij[z]++;
+    }
+}
+eens();
+
+function oneens(x)
+{
+    y = x.split(" ");
+    z = 0;
+    foreach(y)
+    {
+        z++
+        partij[z]++;
+    }
+}
+oneens();
