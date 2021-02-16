4de2c58512b1da3d89e44d8433a58c8ec6269a05
Joshua <Doerak1aug@gmail.com>
2021-02-16 14:52:21 +0100

    Start scherm klaar

--- java.js
+++ java.js
@@ -1,6 +1,19 @@
 UserAwnsers = [];
 inputs = 0;
 progressvar = (100 / (subjects.length + 3));
+
+
+function ButtonStart()
+{
+    inputs++
+    document.getElementById("progressbar").style.width = (inputs*progressvar) + "%";
+    document.getElementById("MainContainer").style.backgroundColor = "whitesmoke";
+    document.getElementById("StartContainer").style.display = "none";
+    document.getElementById("VragenContainer").style.display = "unset";
+    NextQuestion(inputs-1)
+    console.log(inputs)
+}
+
 function AwnserSubmit(x)
 {  
     UserAwnsers[inputs] = x;
@@ -8,14 +21,32 @@ function AwnserSubmit(x)
     console.log(UserAwnsers);
     document.getElementById("progressbar").style.width = (inputs*progressvar) + "%";
     /* console.log(inputs) */
-    NextQuestion(inputs-1);
+    if(inputs <= 30)
+    {
+        NextQuestion(inputs-1);
+    }
+    else if(inputs == 31)
+    {
+        SchermSpecify()
+    }
+    
 }
 function GoBack()
 {
     inputs--;
     document.getElementById("progressbar").style.width = (inputs*progressvar) + "%";
-    /* console.log(inputs) */
-    NextQuestion(inputs-1);
+    console.log(inputs)
+    if(inputs == 0)
+    {
+        document.getElementById("MainContainer").style.backgroundColor = "";
+        document.getElementById("StartContainer").style.display = "unset";
+        document.getElementById("VragenContainer").style.display = "none";
+    }
+    else
+    {
+        NextQuestion(inputs-1);
+    }
+    
 }
 
 function NextQuestion(x)
